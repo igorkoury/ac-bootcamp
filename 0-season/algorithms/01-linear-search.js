3460ab9253a41c9d270c1bf53779ef8dd5c784fa
Igor Koury <87314880+igorkoury@users.noreply.github.com>
2022-03-02 23:39:03 +0000

    linear search implementation

---
+++ 0-season/algorithms/01-linear-search.js
@@ -0,0 +1,17 @@
+
+// A linear search implementation in JavaScript
+
+function linearSearch(arrNum, num){
+
+	for(let i = 0; i < arrNum.length; i++){
+
+		if(arrNum[i] == num){
+			return i;
+		}
+
+	}
+	return false;
+}
+
+
+console.log(linearSearch([4, 6, 1, 8], 1));
