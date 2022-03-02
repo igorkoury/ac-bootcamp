cbde8292600f07b05ef44b09c4777352432d0360
Igor Koury <87314880+igorkoury@users.noreply.github.com>
2022-03-02 23:46:56 +0000

    insetion-sort implementation

---
+++ 0-season/algorithms/03-insertion-sort.js
@@ -0,0 +1,33 @@
+// Exemple of insetion-sort implementation on JavaScript
+
+function insertionSort(arr){
+
+	let orderArr = [];
+	let num = 0;
+	let pos = 0;
+	
+	orderArr.push(arr[0]);
+
+	for(let i = 1; i < arr.length; i++){
+		
+		for(let j = 0; j < orderArr.length; j++){
+			console.log('Valor de J: ' + j);
+			if(arr[i] < orderArr[j]){
+				console.log('Entrei no IF');
+				num = arr[i];
+				pos = j;
+				
+			} else{
+				console.log('Entrei no ELSE');
+				pos = j + 1;
+			}
+
+		}
+		orderArr.splice(pos, 0, arr[i]);
+		console.log(orderArr);
+	}
+return orderArr;
+}
+
+
+console.log(insertionSort([7, 2, 9, 1])); 
